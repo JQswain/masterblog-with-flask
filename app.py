3c91e92d542c4f6c3a55f325a284f4f265d972dd
JQSwain <j.q.swain@hotmail.com>
2025-07-11 17:24:00 +0200

    Added a template for index.html, and a static stylesheet, and rendered a simple html wiht the template and the data from blog_posts

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import flask
+from flask import Flask, render_template
+import json
 
-app = flask.Flask(__name__)
+with open("blog_posts.json", 'r') as blog_posts:
+    blog_posts = json.load(blog_posts)
+
+app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html', posts=blog_posts)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
