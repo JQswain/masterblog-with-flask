32a4c436ce5a798e9d85f3c8a7450198172332f3
JQSwain <j.q.swain@hotmail.com>
2025-07-13 16:32:27 +0200

    added the update feature and fixed the pathways for delete

--- app.py
+++ app.py
@@ -7,11 +7,18 @@ def load_posts():
         with open('blog_posts.json', 'r') as r:
             return json.load(r)
     else:
-        return {}
+        return []
 
 def save_posts(posts):
     with open('blog_posts.json', 'w') as w:
         json.dump(posts, w, indent=4)
+
+def fetch_post(post_id):
+    posts = load_posts()
+    for post in posts:
+        if post['id'] == post_id:
+            return post
+    return None
 
 
 app = Flask(__name__)
@@ -27,9 +34,15 @@ def add():
     if request.method == 'POST':
         blog_posts = load_posts()
 
-        author = request.form['author']
-        title = request.form['title']
-        content = request.form['content']
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        if blog_posts:
+            next_id = max(post['id'] for post in blog_posts) + 1
+        else:
+            next_id = 1
+
 
         post = {
             'id': len(blog_posts) + 1,
@@ -56,7 +69,43 @@ def delete(post_id):
     save_posts(updated_posts)
     return redirect(url_for('index'))
 
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'] )
+def update(post_id):
+    # Fetch the blog posts from the JSON file
+    old_post = fetch_post(post_id)
+    if old_post is None:
+        # Post not found
+        return "Post not found", 404
 
+    if request.method == 'POST':
+        # Update the post in the JSON file
+        all_posts = load_posts()
+
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        new_post = {
+            'id': post_id,
+            'author': author,
+            'title': title,
+            'content': content
+         }
+
+        updated_posts = [post for post in all_posts if post['id'] != post_id]
+        updated_posts.append(new_post)
+
+        updated_posts.sort(key=lambda p: p['id'])
+
+        save_posts(updated_posts)
+
+        # Redirect back to index
+        return redirect(url_for('index'))
+
+    # Else, it's a GET request
+    else:
+    # So display the update.html page
+        return render_template('update.html', post=old_post)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
